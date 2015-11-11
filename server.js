ed2eb68534441b698a704819fde1bf87e288d35b
Takeru Komiya <tkomiya@hotmail.com>
2015-11-10 20:53:12 -0500

    Removes server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-//app.use(serveStatic("/Users/takeru/Sites/sportsstore"));
-app.use(serveStatic("./"));
-app.listen(5000);
