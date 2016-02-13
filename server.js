ded21f45d92eb9f346bb590144457ec374c0516a
Takeru Komiya <tkomiya@hotmail.com>
2016-02-13 08:32:32 -0800

    Refactors code into methods

--- server.js
+++ server.js
@@ -3,11 +3,17 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+function configureExpressApp() {
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static(__dirname + '/public'));
+  app.use('/bower_components', express.static(__dirname + '/bower_components'));
+}
 
-app.use(express.static(__dirname + '/public'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+function startExpressApp() {
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+}
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+configureExpressApp();
+startExpressApp();
