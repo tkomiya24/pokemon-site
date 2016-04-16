6497342b14a25c78a1a2b47b48dde43f66ce7b06
Takeru Komiya <tkomiya@hotmail.com>
2016-04-16 16:30:58 -0700

    Uses new routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var db = require('./db/connect');
+var router = require('./config/routes');
 
 function configureExpressApp() {
   app.set('port', (process.env.PORT || 5000));
@@ -11,6 +12,7 @@ function configureExpressApp() {
 }
 
 function startExpressApp() {
+  app.use('/', router);
   app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
