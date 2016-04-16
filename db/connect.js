bba6d2caa34a43ec99793e6a433d7bdc6dc2dee6
Takeru Komiya <tkomiya@hotmail.com>
2016-04-16 14:49:40 -0700

    Adds a config for a mongolab remote db

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,11 @@
 var mongoose = require('mongoose');
 
 module.exports.connectToDatabase = function(callback) {
-  mongoose.connect('mongodb://localhost/pokemon-site');
+  if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  } else {
+    mongoose.connect('mongodb://localhost/pokemon-site');
+  }
   mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
   mongoose.connection.once('open', callback);
 };
