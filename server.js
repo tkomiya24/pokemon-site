09ff2c3e520c9773787b411608b94ba69f2d76ef
Takeru Komiya <tkomiya@hotmail.com>
2016-04-17 19:16:59 -0700

    Adds passport and dependent middlewares to express

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ var express = require('express');
 var app = express();
 var db = require('./db/connect');
 var router = require('./config/routes');
+var passport = require('passport');
+var authentication = require('./config/authentication');
 
 function configureExpressApp() {
+  authentication.configure();
   app.set('port', (process.env.PORT || 5000));
   app.use(express.static(__dirname + '/public'));
   app.use('/bower_components', express.static(__dirname + '/bower_components'));
+  app.use(require('cookie-parser')());
+  app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 }
 
 function startExpressApp() {
