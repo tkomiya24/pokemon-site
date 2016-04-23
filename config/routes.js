3de3b5df891a9f4c103c79c69b6d77c6cff34793
Takeru Komiya <tkomiya@hotmail.com>
2016-04-23 13:49:47 -0700

    Adds a route for getting pokemon

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var usersController = require('../app/controllers/usersController');
+var pokemonController = require('../app/controllers/pokemonController');
 
 router.route('/users')
   .post(usersController.createOne);
@@ -9,5 +10,8 @@ router.route('/user')
   .get(usersController.getAuthenticatedUser)
   .patch(usersController.update)
   .delete(usersController.delete);
+
+router.route('/pokemon')
+  .get(pokemonController.getAll);
 
 module.exports = router;
