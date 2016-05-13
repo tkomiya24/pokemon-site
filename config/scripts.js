2dc11fda3d1ba4d30e80696c836f5b95a8b5e2b7
Takeru Komiya <tkomiya@hotmail.com>
2016-05-13 11:30:34 -0700

    Adds a clientJs property which removes the public prefix

--- config/scripts.js
+++ config/scripts.js
@@ -1,5 +1,15 @@
+'use strict';
+
 var glob = require('glob');
+var publicJs = glob.sync('./public/**/*.js');
+
+function removePrefix(fileNames) {
+  return fileNames.map(function(name) {
+    return name.replace(/^\.\/public/, '');
+  });
+}
 
 module.exports = {
-  publicJs: glob.sync('./public/**/*.js')
+  publicJs: publicJs,
+  clientJs: removePrefix(publicJs)
 };
