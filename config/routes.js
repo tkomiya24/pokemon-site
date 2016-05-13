175a976e1cece8d8582c25bbaa1980f659b9a6e2
Takeru Komiya <tkomiya@hotmail.com>
2016-05-13 11:14:27 -0700

    Adds a route for root and renders index.ejs

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,14 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 var usersController = require('../app/controllers/usersController');
 var pokemonController = require('../app/controllers/pokemonController');
+var scripts = require('./scripts');
+
+router.route('/')
+  .get(function(req, res, next) {
+    res.render('index', {scripts: scripts.publicJs});
+  });
 
 router.route('/users')
   .post(usersController.createOne);
